21e42119ccf47a36e4a0086392e7385f0907cdb0
Lucas Bocanegra <lucasfernandobocanegra@gmail.com>
2017-12-15 00:22:36 -0200

    add icon

--- src/components/template/Sidebar.js
+++ src/components/template/Sidebar.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Collapse, Well } from 'react-bootstrap';
+import { Collapse, Well, Glyphicon, FormGroup, FormControl, InputGroup } from 'react-bootstrap';
 
 import '../../assets/stylesheet/sidebar.css';
 
@@ -18,7 +18,10 @@ class SideBarItem extends Component {
     render() {
         return (
             <li key={this.props.index} className={this.props.active ? "active" : "not-active"}>
-                <a href="#">{this.props.alias}</a>
+                <a href="#">
+                <Glyphicon glyph={this.props.icon} />
+                {this.props.value}
+                </a>
             </li>
         );
     }
@@ -37,8 +40,8 @@ class SideBarItemExtended extends Component {
                 <a  href="#"
                     expanded={this.state.open ? "true" : "false"}
                     onClick={() => this.setState({ open: !this.state.open })}>
-
-                    {this.props.alias}
+                    <Glyphicon glyph={this.props.icon} />
+                    {this.props.value}
                 </a>
                 <Collapse in={this.state.open} >
                     <div>
@@ -49,7 +52,7 @@ class SideBarItemExtended extends Component {
                                         this.props.subItems.map(item =>{
                                             return (
                                                 <SideBarItem key={item.index} 
-                                                             alias={item.alias} />
+                                                             value={item.value} />
                                             );
                                         })
                                         : null
@@ -71,21 +74,32 @@ class SideBar extends Component {
                 {/*  Sidebar Header */}
                 <SidebarHeader title="Flex Forward" />
 
+                <FormGroup>
+                    <InputGroup>
+                        <FormControl type="text" placeholder="Pesquisar" />
+                        <InputGroup.Addon>
+                            <Glyphicon glyph="search" />
+                        </InputGroup.Addon>
+                    </InputGroup>
+                </FormGroup>
+
                 {/* Sidebar Links */}
                 <ul className="list-unstyled components">
 
-                    <SideBarItem index="home" alias="Home" active="true" />
-                    <SideBarItem index="sobre" alias="Sobre" />
+                    <SideBarItem index="home" value="Home" active="true" icon="home" />
+                    <SideBarItem index="sobre" value="Sobre" icon="question-sign" />
                     <SideBarItemExtended 
                             index="paginas"
-                            alias="Páginas"
+                            value="Páginas"
+                            icon="folder-close"
                             subItems={[
-                                {index: "sub1", alias: "Sub Menu 1"},                       
-                                {index: "sub2", alias: "Sub Menu 2"},                       
-                                {index: "sub3", alias: "Sub Menu 3"}                       
+                                {index: "sub1", value: "Sub Menu 1"},                       
+                                {index: "sub2", value: "Sub Menu 2"},                       
+                                {index: "sub3", value: "Sub Menu 3"}                       
                             ]} /> 
-                    <SideBarItem index="portifolio" alias="Portfolio" />
-                    <SideBarItem index="contato" alias="Contato" />
+
+                    <SideBarItem index="portifolio" value="Portfolio" icon="align-justify"  />
+                    <SideBarItem index="contato" value="Contato" icon="envelope" />
                 </ul>
             </nav>
         );
